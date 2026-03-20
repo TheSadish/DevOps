cbdfa5ce7770fbdf8c6774da6e3f942f67bdf767
Sadish Kumar Dash <59526333+TheSadish@users.noreply.github.com>
2026-03-20 16:24:31 +0530

    Add EKS variables for region and node configuration

---
+++ terraform/eks/variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "cluster_name" {
+  default = "my-eks-cluster"
+}
+
+variable "node_instance_type" {
+  default = "t3.medium"
+}
+
+variable "desired_capacity" {
+  default = 2
+}
+
+variable "max_capacity" {
+  default = 3
+}
+
+variable "min_capacity" {
+  default = 1
+}
